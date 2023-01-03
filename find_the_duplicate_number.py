b7157b8db26782049301158d129ff591b940a8c3
Arjun Sivakumar <43909235+ArjunS18@users.noreply.github.com>
2023-01-03 10:37:20 -0500

    Create find_the_duplicate_number.py

---
+++ find_the_duplicate_number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dupset = {}
+        dupset = set()
+        for i in range(0, len(nums)):
+            if nums[i] not in dupset:
+                dupset.add(nums[i])
+            else:
+                return nums[i]
+    
